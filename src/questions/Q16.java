0c226fd4c69cb640a726d4d2936909b3a2ed625f
1105213969 <1105213969>
2019-11-24 22:15:30 +0800

    Q17 树的子结构

---
+++ src/questions/Q16.java
@@ -0,0 +1,32 @@
+package questions;
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+/**
+ * 合并两个有序链表
+ */
+public class Q16 {
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        ListNode newList = new ListNode(0);
+        ListNode p = newList;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                p.next = list1;
+                list1 = list1.next;
+            } else {
+                p.next = list2;
+                list2 = list2.next;
+            }
+            p = p.next;
+        }
+        p.next = (list1 == null) ? list2 : list1;
+        return newList.next;
+    }
+}
